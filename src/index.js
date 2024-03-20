b6c51eeac531a73868d9909a16db1531ced9e0c2
vier <oliviervroege@hotmail.com>
2024-03-20 08:04:43 +0100

    elements removed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
-import Elements from "./pages/Elements";
 import MerchStore from "./pages/MerchStore";
 import NoPage from "./pages/NoPage";
 import './index.css'
@@ -14,7 +13,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="elements" element={<Elements />} />
           <Route path="merchstore" element ={<MerchStore />} />
           <Route path="*" element={<NoPage />} />
         </Route>
