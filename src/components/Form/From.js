294d509c1d607bdfddd14b54ae43a86497d18f3e
vier <oliviervroege@hotmail.com>
2024-03-20 15:22:19 +0100

    added selectbox for countries

--- src/components/Form/From.js
+++ src/components/Form/From.js
@@ -12,6 +12,8 @@ const Form = ({ array, total }) => {
     const [email, setEmail] = useState('');
     const [consent, setConsent] = useState(false);
 
+    const countryList = ["Switzerland", "Germany", "Austria", "Netherlands", "France", "Italy", "Spain", "Portugal", "Greece", "United Kingdom", "Ireland", "Sweden", "Norway", "Denmark", "Finland", "Poland", "Czech Republic", "Slovakia", "Hungary", "Slovenia", "Croatia", "Bosnia and Herzegovina", "Serbia", "Montenegro", "Albania", "Macedonia", "Bulgaria", "Romania", "Moldova", "Ukraine", "Belarus", "Russia", "Estonia", "Latvia", "Lithuania", "Belgium", "Luxembourg", "Liechtenstein", "Monaco", "Andorra", "San Marino", "Vatican City", "Malta", "Cyprus",]
+
     const handleSubmit = (e) => {
         e.preventDefault();
         alert(`Thank you for your order:
@@ -30,7 +32,7 @@ const Form = ({ array, total }) => {
 
     return (
         <form onSubmit={handleSubmit} className='grid grid-cols-2 gap-2 p-4 m-4'>
-                        <div className='flex justify-start mx-2 gap-4 col-span-2'>
+            <div className='flex justify-start mx-2 gap-4 col-span-2'>
                 <div>
                     <input className='mr-2' type="radio" id="male" value="Male" name="gender" onChange={(e) => setGender(e.target.value)} />
                     <label htmlFor="male">Male</label>
@@ -40,23 +42,28 @@ const Form = ({ array, total }) => {
                     <label htmlFor="female">Female</label>
                 </div>
                 <div>
-                    <input  className='mr-2' type="radio" id="others" value="Other" name="gender" onChange={(e) => setGender(e.target.value)} />
+                    <input className='mr-2' type="radio" id="others" value="Other" name="gender" onChange={(e) => setGender(e.target.value)} />
                     <label htmlFor="others">Others</label>
                 </div>
             </div>
-                <input className='mx-2 rounded-md text-black' type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder='Name'/>
-                <input className='rounded-md mx-2 text-black' type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder='Firstname' />
-                <input className='rounded-md mx-2 text-black' type="text" value={street} onChange={(e) => setStreet(e.target.value)} placeholder='Street' />
-                <input className='rounded-md mx-2 text-black' type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder='City' />
-                <input className='rounded-md mx-2 text-black' type="text" value={zipcode} onChange={(e) => setZipcode(e.target.value)} placeholder='Zip Code' />
-                <input className='rounded-md mx-2 text-black' type="text" value={country} onChange={(e) => setCountry(e.target.value)} placeholder='Country' />
-                <input className='rounded-md mx-2 text-black' type="text" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} placeholder='Phone Number' />
-                <input className='rounded-md mx-2 text-black' type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' />
-                <div className='flex justify-between mx-2 gap-4'>
+            <input className='mx-2 rounded-md text-black' type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder='Name' />
+            <input className='rounded-md mx-2 text-black' type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder='Firstname' />
+            <input className='rounded-md mx-2 text-black' type="text" value={street} onChange={(e) => setStreet(e.target.value)} placeholder='Street' />
+            <input className='rounded-md mx-2 text-black' type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder='City' />
+            <input className='rounded-md mx-2 text-black' type="text" value={zipcode} onChange={(e) => setZipcode(e.target.value)} placeholder='Zip Code' />
+            <select className='rounded-md mx-2 text-black' value={country} onChange={(e) => setCountry(e.target.value)} required>
+                <option value="" disabled>Country</option>
+                {countryList.map((country, index) => (
+                    <option key={index} value={country}>{country}</option>
+                ))}
+            </select>
+            <input className='rounded-md mx-2 text-black' type="text" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} placeholder='Phone Number' />
+            <input className='rounded-md mx-2 text-black' type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' />
+            <div className='flex justify-between mx-2 gap-4'>
                 <input className='place-self-center rounded-md' type="checkbox" checked={consent} onChange={(e) => setConsent(e.target.checked)} required />
                 <label className='text-white'>I agree to the terms and conditions</label>
-                </div>
-                <button className='place-self-center bg-gradient-to-r from-gray-700 to-indigo-900 hover:to-indigo-950 hover:from-gray-950 border-2 border-white rounded-md p-2' type="submit">Bestellen</button>
+            </div>
+            <button className='place-self-center bg-gradient-to-r from-gray-700 to-indigo-900 hover:to-indigo-950 hover:from-gray-950 border-2 border-white rounded-md p-2' type="submit">Bestellen</button>
         </form>
     );
 };
